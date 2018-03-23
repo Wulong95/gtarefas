039b137880aa102baea73faaca2a1be0444aebea
Fabio Augusto Pinheiro <fabinhowulong@gmail.com>
2018-03-23 16:02:18 -0300

    Criação das Rotas Criação do Page Header Configuração das páginas de Tarefas e Sobre

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,15 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
-import Sobre from '../sobre/sobre'
+import Routes from './routes'
 import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
         <Menu />
-        <Todo />
-        <Sobre />
+        <Routes />
     </div>
 )
  
--- frontend/src/sobre/sobre.jsx
+++ frontend/src/sobre/sobre.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Quem Somos'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
     </div>
 )
